40af4d16992e3b4e5fda7b32a9c5f483569385e6
SkliarovaAnastasiia <skliarova16@gmail.com>
2024-05-14 14:10:43 +0300

    updates

--- src/app/lib/utils/QueryClient.ts
+++
@@ -1,5 +0,0 @@
-import { cache } from "react";
-import { QueryClient } from "react-query";
-
-const getQueryClient = cache(() => new QueryClient()) as () => QueryClient;
-export default getQueryClient;
