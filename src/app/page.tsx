2046a36cb83433956aed39dd5ed46f5e4173687a
SkliarovaAnastasiia <skliarova16@gmail.com>
2024-05-14 13:39:59 +0300

    fixed bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { QueryClientProvider } from "react-query";
-import queryClient from "@/app/lib/utils/QueryClient";
 import CharactersList from "@/app/components/CharactersList";
 
 export default function Home() {
   return (
-    <QueryClientProvider client={queryClient()}>
-      <main className="flex min-h-screen flex-col items-center justify-between">
-        <CharactersList></CharactersList>
-      </main>
-    </QueryClientProvider>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <CharactersList />
+    </main>
   );
 }
